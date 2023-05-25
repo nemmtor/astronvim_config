957f0f8befc3a9f3055e52c4f59abe3ae70fd984
nemmtor <kacper0witas@gmail.com>
2023-05-25 23:50:43 +0200

    add undo tree and unmap things i dont like

--- mappings.lua
+++ mappings.lua
@@ -1,37 +1,63 @@
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
+local utils = require "astronvim.utils"
+local get_icon = utils.get_icon
+
+local sections = {
+  s = { desc = get_icon("Search", 1, true) .. "[S]earch" },
+}
+
 return {
-  -- first key is the mode
   n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick to close",
-    },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- uncomment things that I don't use
+    ["<leader>e"] = false,
+    ["<leader>h"] = false,
+    ["<leader>n"] = false,
+    ["<leader>o"] = false,
+    ["<leader>q"] = false,
+    ["<leader>w"] = false,
+    ["<leader>f"] = false,
+    ["<leader>f<CR>"] = false,
+    ["<leader>f'"] = false,
+    ["<leader>fa"] = false,
+    ["<leader>fb"] = false,
+    ["<leader>fc"] = false,
+    ["<leader>fC"] = false,
+    ["<leader>ff"] = false,
+    ["<leader>fF"] = false,
+    ["<leader>fh"] = false,
+    ["<leader>fk"] = false,
+    ["<leader>fm"] = false,
+    ["<leader>fo"] = false,
+    ["<leader>fr"] = false,
+    ["<leader>ft"] = false,
+    ["<leader>fw"] = false,
+    ["<leader>fW"] = false,
+    ["<leader>fn"] = false,
+    ["<leader>/"] = false,
+    ["<C-'>"] = false,
     ["J"] = { "mzJ`z", desc = "Better merge lines" },
     ["n"] = { "nzzzv", desc = "Better search next" },
     ["N"] = { "Nzzzv", desc = "Better search prev" },
+    ["<leader>s"] = sections.s,
+    ["<leader>sg"] = { function() require("telescope.builtin").live_grep() end, desc = "[S]earch [G]rep" },
+    ["<leader>sf"] = {
+      function() require("telescope.builtin").find_files { hidden = false } end,
+      desc = "[S]earch [F]iles",
+    },
+    ["<leader>?"] = { function() require("telescope.builtin").oldfiles() end, desc = "[?] Search recent files" },
+    ["<F1>"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
+    ["<F3>"] = {
+      function()
+        vim.cmd.UndotreeToggle()
+        vim.cmd.UndotreeFocus()
+      end,
+      desc = "Toggle Explorer1",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
   v = {
-    -- move lines up down by using shift+J/shift+K in visual mode
     ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line up" },
     ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line down" },
   },
