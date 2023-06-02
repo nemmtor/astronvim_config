84ffd12ee0b1e69fe6221ce6ad88db3122209445
nemmtor <kacper0witas@gmail.com>
2023-06-02 20:06:21 +0200

    add neo tree tweaks

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,10 +1,16 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
+    opts.filesystem.hijack_netrw_behavior = "open_default"
     opts.filesystem.filtered_items = {
       visible = true,
       hide_dotfiles = false,
       hide_gitignored = false,
     }
+    opts.window.position = "right"
+    opts.window.width = 60
+    opts.window.mappings["<cr>"] = "open_with_window_picker"
+    opts.window.mappings["s"] = "split_with_window_picker"
+    opts.window.mappings["S"] = "vsplit_with_window_picker"
   end,
 }
