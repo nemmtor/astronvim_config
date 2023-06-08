f2e8cbac7792fc5a7b64fcec84677bcb93cd2793
nemmtor <kacper0witas@gmail.com>
2023-06-08 12:12:17 +0200

    tweak neo tree settings

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -3,18 +3,17 @@ return {
   opts = function(_, opts)
     opts.filesystem.hijack_netrw_behavior = "open_default"
     opts.filesystem.filtered_items = {
-      visible = true,
-      hide_dotfiles = false,
-      hide_gitignored = false,
+      visible = false,
+      hide_dotfiles = true,
+      hide_gitignored = true,
     }
-    opts.window.position = "right"
-    opts.window.width = 60
+    opts.window.position = "left"
+    opts.window.width = 40
     opts.window.mappings["<cr>"] = "open_with_window_picker"
     opts.window.mappings["s"] = "split_with_window_picker"
     opts.window.mappings["S"] = "vsplit_with_window_picker"
     opts.source_selector.sources = {
-      { source = "filesystem", display_name = " 󰉓 Files" },
-      { source = "git_status", display_name = " 󰊢 Git" },
+      { source = "filesystem", display_name = "󰉓 Files" },
     }
   end,
 }
